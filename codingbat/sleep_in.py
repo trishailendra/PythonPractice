40d578b4ec8e8c450b7369e46376ac4e8c0c442c
trishailendra <trip.shailendra@gmail.com>
2024-01-21 16:33:26 +0530

    Update sleep_in.py

--- codingbat/sleep_in.py
+++ codingbat/sleep_in.py
@@ -167,3 +167,73 @@ def pos_neg(a, b, negative):
     else:
         return False
 print(pos_neg(-1,-1,False))
+
+
+'''
+Given a string, return a new string where "not " has been added to the front. However, 
+if the string already begins with "not", return the string unchanged.
+
+
+not_string('candy') → 'not candy'
+not_string('x') → 'not x'
+not_string('not bad') → 'not bad'
+
+'''
+def not_string(str):
+    if str.startswith("not")!= True:
+        return "not " + str
+    else:
+        return str
+print(not_string("not candy"))
+
+'''
+missing_char:
+
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+'''
+def missing_char(str, n):
+    return str[:n] + str[n+1:]
+
+print(missing_char("sorry",1))
+
+
+'''
+front_back:
+
+Given a string, return a new string where the first and last chars have been exchanged.
+
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+
+'''
+
+def front_back(str):
+    if len(str)==1:
+        return str
+    else:
+        return str[-1:]+ str[1:-1] +str[:1]
+
+print(front_back("C"))
+
+'''
+front3:
+
+Given a string, we'll say that the front is the first 3 chars of the string. 
+If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+front3('Java') → 'JavJavJav'
+front3('Chocolate') → 'ChoChoCho'
+front3('abc') → 'abcabcabc'
+'''
+def front3(str):
+    return str[:3]*3
+
+print(front3("te"))
