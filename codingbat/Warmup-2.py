065b2d327c837dd9afe064e53aa7b20f92b7c6a8
trishailendra <trip.shailendra@gmail.com>
2024-01-23 11:55:28 +0530

    Practice

---
+++ codingbat/Warmup-2.py
@@ -0,0 +1,63 @@
+'''
+string_times:
+
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+'''
+def string_times(str, n):
+    return str*n
+
+print(string_times("lucky",4))
+
+'''
+front_times:
+
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, 
+or whatever is there if the string is less than length 3. Return n copies of the front;
+
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+'''
+
+def front_times(str, n):
+    return str[:3]*n
+
+print (front_times("coding",3))
+
+'''
+string_bits:
+
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+string_bits('Hello') → 'Hlo'
+string_bits('Hi') → 'H'
+string_bits('Heeololeo') → 'Hello'
+'''
+
+def string_bits(str):
+   return str[::2]
+
+print(string_bits("H"))
+
+
+'''
+string_splosion:
+
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+string_splosion('Code') → 'CCoCodCode'
+string_splosion('abc') → 'aababc'
+string_splosion('ab') → 'aab'
+'''
+def string_splosion(str):
+    for x in len(str)<0:
+        return str[x]
+print(string_splosion("code"))
+
