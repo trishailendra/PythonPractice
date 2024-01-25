354ecd37cf0b671f695d8abd089afd83128aea0d
trishailendra <trip.shailendra@gmail.com>
2024-01-25 17:07:12 +0530

    Practice

---
+++ codingbat/first_half.py
@@ -0,0 +1,15 @@
+'''
+first_half:
+
+
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+
+first_half('WooHoo') → 'Woo'
+first_half('HelloThere') → 'Hello'
+first_half('abcdef') → 'abc'
+'''
+def first_half(str):
+    return str[:len(str)//2]
+
+print(first_half("HelloThere"))
