2cd14463cef61a38d47180c24f1a9f81ffe1d04c
trishailendra <trip.shailendra@gmail.com>
2024-01-26 17:12:57 +0530

    Practice

---
+++ codingbat/same_first_last.py
@@ -0,0 +1,15 @@
+'''
+same_first_last:
+
+Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+
+same_first_last([1, 2, 3]) → False
+same_first_last([1, 2, 3, 1]) → True
+same_first_last([1, 2, 1]) → True
+'''
+def same_first_last(nums):
+    if nums!=[] and nums[0]==nums[-1]:
+        return True
+    return False
+print(same_first_last([1, 2, 1]))
